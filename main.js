80e879c36569fa13b3a3c503cdc2a08f6b4c09db
Aaron Alexander <a.alexander72003@gmail.com>
2021-08-11 16:31:30 -0500

    First draft of RPS project; created basic function for game and basic if-else loops

---
+++ main.js
@@ -0,0 +1,44 @@
+function computerPlay() {
+    var playArray = [
+       "Rock",
+       "Paper",
+       "Scissors"
+    ];
+    var randomNumber = Math.floor(Math.random()*playArray.length);
+    return playArray[randomNumber];
+}
+function playRound(playerSelection, computerSelection) {
+    var x = playerSelection.toUpperCase();
+    var y = computerSelection.toUpperCase();
+
+if (x == "ROCK" && y == "SCISSORS") {
+    return "You Win! Rock beats Scissors!";
+}
+else if (x == "PAPER" && y == "ROCK") {
+    return "You Win! Paper beats Rock!";
+}
+else if (x == "SCISSORS" && y == "PAPER") {
+    return "You Win! Scissors beats Paper!"
+}
+
+if (x == "ROCK" && y == "PAPER") {
+    return "You Lose! Paper beats Rock!";
+}
+else if (x == "PAPER" & y == "SCISSORS") {
+    return "You Lose! Scissors beats Paper!";
+}
+else if (x == "SCISSORS" && y == "ROCK") {
+    return "You Lose! Rock beats Scissors!"
+}
+
+if (x == "ROCK" && y == "ROCK") {
+    return "It's a Draw! Try again!";
+}
+else if (x == "PAPER" && y == "PAPER") {
+    return "It's a Draw! Try again!";
+}
+else if (x == "SCISSORS" && y == "SCISSORS") {
+    return "It's a Draw! Try again!"
+}
+else return "Please enter your choices!";
+}
